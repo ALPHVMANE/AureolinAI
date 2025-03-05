e996e02f5771f7cf12eed02661c21542aa726559
ALPHVMANE <alphxrizen@gmail.com>
2025-03-05 16:52:37 -0500

    Update config.php
    reconfig_imggen

--- config/API/imggen.ai/config.php
+++ config/API/imggen.ai/config.php
@@ -1,5 +1,25 @@
 <?php
 require_once "env.php";
+
+function callAPI($method, $url, $data) {
+    $curl = curl_init();
+    switch (strtoupper($method)) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data) 
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        default:
+            if ($data) 
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    // OPTIONS:   curl_setopt($curl, CURLOPT_URL, $url);   curl_setopt($curl, CURLOPT_HTTPHEADER, array(      'APIKEY: 111111111111111111111',      'Content-Type: application/json',   ));   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);   // EXECUTE:   $result = curl_exec($curl);   if(!$result){die("Connection Failure");}   curl_close($curl);   return $result;}
+}
 function generateImage($prompt, $aspect_ratio = "square") {
     $apiKey = IMGGEN_APIK;
     $url = IMGGEN_URL;
