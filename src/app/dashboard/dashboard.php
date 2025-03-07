2b298b51a2aa47b4704e0a444c708bb8743e8276
Tim <vuluongquy2003@gmail.com>
2025-03-07 09:36:35 -0500

    dashboard.php

--- src/app/dashboard/dashboard.php
+++ src/app/dashboard/dashboard.php
@@ -1,10 +1,11 @@
 <?php
+//do not delete this part of code
 session_start();
 if (!isset($_SESSION['username'])) {
     header("Location: ../../../public/index.php?error=Please login first!");
     exit();
 }
-
+//Everything below is deletable
 echo "Welcome, " . $_SESSION['username'] . "! <a href='../auth/logout.php'>Logout</a>";
 echo " | <a href='../../features/faceid/setupfaceid.php'>Set up Face ID</a>";
 ?>
