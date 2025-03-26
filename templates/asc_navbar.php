0f278c5c8473dc589986b8e2e9c04f12da9bbb3c
Ascace <alexander.santa.cruz@hotmail.com>
2025-03-25 22:39:35 -0400

    create asc_navbar.php
    asc_navbar.php

---
+++ templates/asc_navbar.php
@@ -0,0 +1,32 @@
+<nav class="navbar">
+    <div class="logo">
+        <a href="asc_index.php">Aureolin</a>
+    </div>
+    <ul class="nav-links">
+        <li><a href="asc_index.php">Home</a></li>
+        
+        <li><a href="">About Us</a></li>
+        <li><a href="">Image Generation</a></li>
+        <li class="dropdown">
+            <a href="#">Profile ▼</a>
+            <ul class="dropdown-menu">
+                <li><a href="asc_settings.php">Settings</a></li>
+                <li><a href="asc_leaderboard.php">Leaderboards</a></li>
+                <li><a href="asc_logout.php">Logout</a></li>
+            </ul>
+        </li>
+        <li class="dropdown">
+            <a href="#">Learning ▼</a>
+            <ul class="dropdown-menu">
+                <li><a href="asc_gamestart.php">NewGame</a></li>
+                <li><a href="asc_resume.php">LoadGame</a></li>
+            </ul>
+        </li>
+    </ul>
+    
+    <div class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</nav>
